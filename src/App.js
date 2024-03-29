c8cf3c9bce677743f9423d6b4c981a531fd2a39b
Manish201304 <107488431+Manish201304@users.noreply.github.com>
2024-03-29 17:41:04 +0530

    useContext hook: Using React Context API

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import './App.css';
 import About from './Components/About';
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
+import NoteState from './context/notes/NoteState';
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about"element={<About />} />
-          {/* <Route path="/user" element={<About />} /> */}
-        </Routes>
-      </div>
-    </Router>
+    <NoteState> 
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
+    </NoteState>
   );
 }
 
