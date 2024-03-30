52ebc09678bc9295f2994f6e573b388f4d611f1a
Manish201304 <107488431+Manish201304@users.noreply.github.com>
2024-03-30 10:45:39 +0530

    useLocation Hook, Fetching Notes from Context

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   return (
     <NoteState> 
       <Router>
-        <div>
           <Navbar />
+        <div className='container'>
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/about" element={<About />} />
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,23 +1,40 @@
-import NoteContext from "./noteContext";
 import { useState } from "react";
+import NoteContext from "./noteContext";
 
-const NoteState = (props)=>{
-    const s1 = {
-        "name": "Manish",
-        "course": "MCA"
-    }
-     const [state, setState] = useState(s1)
-     const update = ()=>{
-        setTimeout(() => {
-            setState({
-                "name" : "Rajnikant",
-                "course": "BCA"
-            })
-        }, 2000);
-     }
-     
-    return(
-        <NoteContext.Provider value={{state, update}}>
+const NoteState = (props) => {
+    const notesInitail = [
+        {
+            "_id": "6605d1bbf3825e189d94ddb2",
+            "user": "660546dfc54fbfb142c7980b",
+            "title": "Hello",
+            "description": "This is my first notes",
+            "tag": "Nothing",
+            "date": "2024-03-28T20:23:23.528Z",
+            "__v": 0
+        },
+        {
+            "_id": "66079c2b46ada13d83588a51",
+            "user": "660546dfc54fbfb142c7980b",
+            "title": "Operating System",
+            "description": "This is my first notes on OS",
+            "tag": "OS",
+            "date": "2024-03-30T04:59:23.554Z",
+            "__v": 0
+        },
+        {
+            "_id": "66079c2c46ada13d83588a53",
+            "user": "660546dfc54fbfb142c7980b",
+            "title": "Operating System",
+            "description": "This is my first notes on OS",
+            "tag": "OS",
+            "date": "2024-03-30T04:59:24.805Z",
+            "__v": 0
+        }
+    ]
+
+    const [notes, setnotes] = useState(notesInitail)
+    return (
+        <NoteContext.Provider value={{ notes, setnotes }}>
             {props.children}
         </NoteContext.Provider>
     )
